5bdff66e5e5de41bfb5dceb05bc4144ca626d300
oscarN <moskr_2012@hotmail.com>
2020-06-09 01:45:43 -0400

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyBYLZVdzVutgS5kG9C0ISunCJ9TLcrMYpQ",
+    authDomain: "test-angular-5bf43.firebaseapp.com",
+    databaseURL: "https://test-angular-5bf43.firebaseio.com",
+    projectId: "test-angular-5bf43",
+    storageBucket: "test-angular-5bf43.appspot.com",
+    messagingSenderId: "747143952366",
+    appId: "1:747143952366:web:736d176de11d3ceda6c0ff"
+  }
 };
